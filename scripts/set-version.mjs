5154fbb4a572d5e88c89f254c8e96ce459d3c0dd
Morten Olsen <fbtijfdq@void.black>
2024-01-12 14:42:52 +0100

    ci: add NPM publish

--- scripts/set-version.mjs
+++ scripts/set-version.mjs
@@ -1,6 +1,8 @@
 import { findWorkspacePackages } from '@pnpm/find-workspace-packages';
-import { writeFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
+
+const sharedData = JSON.parse(await readFile(join(process.cwd(), 'scripts/shared-data.json')));
 
 const version = process.argv[2];
 if (!version) {
@@ -11,6 +13,9 @@ const packages = await findWorkspacePackages(process.cwd());
 
 for (const { manifest, dir } of packages) {
   console.log(dir, version);
+  for (let [key, value] of Object.entries(sharedData || {})) {
+    manifest[key] = value;
+  }
   manifest.version = version;
   await writeFile(join(dir, 'package.json'), JSON.stringify(manifest, null, 2));
 }
