eeaad68f6edb254f2ba62ee0ad5aa21aec9061cb
Morten Olsen <fbtijfdq@void.black>
2024-01-14 09:45:42 +0100

    feat: initial policy system and version (#29)
    Resolves #22 #24 and #26

--- packages/cli/src/client/client.ts
+++ packages/cli/src/client/client.ts
@@ -2,6 +2,7 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import superjson from 'superjson';
 import type { Runtime } from '@morten-olsen/mini-loader-server';
 import type { RootRouter } from '@morten-olsen/mini-loader-server';
+import pkg from '../../package.json';
 import { Context } from '../context/context.js';
 
 const createClient = (context: Context) => {
@@ -14,6 +15,7 @@ const createClient = (context: Context) => {
       httpBatchLink({
         url: `${context.host}/trpc`,
         headers: {
+          'x-version': pkg.version,
           authorization: `Bearer ${context.token}`,
         },
       }),
