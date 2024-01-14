d01345a1d9fba908d03a52ccb9f17a21da40b4cf
Morten Olsen <fbtijfdq@void.black>
2024-01-14 09:36:47 +0100

    feat: local secrets

---
+++ packages/cli/src/commands/local/local.utils.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+const ENV_PREFIX = 'ML_S_';
+
+const readSecrets = async () => {
+  let secretLocation = join(process.cwd(), '.secret');
+
+  let secrets: Record<string, string> = {};
+
+  if (existsSync(secretLocation)) {
+    const content = await readFile(secretLocation, 'utf-8');
+    secrets = dotenv.parse(content);
+  }
+  for (const key in process.env) {
+    if (key.startsWith(ENV_PREFIX)) {
+      secrets[key.replace(ENV_PREFIX, '')] = process.env[key]!;
+    }
+  }
+  return secrets;
+};
+
+export { readSecrets };
